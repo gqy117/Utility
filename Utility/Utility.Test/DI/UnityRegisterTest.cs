5ceedf9109aeb58ea49c99ae191a036b4653a97a
gqy117 <gqy117@gmail.com>
2014-11-16 11:29:21 +0800

    [master] Added unit tests for UnityRegister.

---
+++ Utility/Utility.Test/DI/UnityRegisterTest.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Utility.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using FluentAssertions;
+    using System.Linq;
+    using Microsoft.Practices.Unity;
+    using Utility.UnityRegister;
+
+    [TestClass]
+    public class UnityRegisterTest
+    {
+        [TestMethod]
+        public void ReadStringTest()
+        {
+            // Arrange
+            IUnityContainer unityContainer = new UnityContainer();
+
+            // Act
+            UnityRegister.RegisterTypes(unityContainer);
+
+            // Assert
+            unityContainer.Resolve<FileReader>().Should().NotBeNull();
+        }
+    }
+}
