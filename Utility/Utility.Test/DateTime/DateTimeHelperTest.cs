d29a49324d9450bad30f5e7422140bf7eb27a967
Leo Li <gqy117@gmail.com>
2017-11-08 18:02:27 +1300

    [master] Added "FiveYears".

--- Utility/Utility.Test/DateTime/DateTimeHelperTest.cs
+++ Utility/Utility.Test/DateTime/DateTimeHelperTest.cs
@@ -11,6 +11,7 @@
         public static IEnumerable<object[]> OneYear => new[] { new object[] { new DateTime(2017, 1, 1), new DateTime(2018, 1, 1), 1M } };
         public static IEnumerable<object[]> HalfYear => new[] { new object[] { new DateTime(2016, 1, 1), new DateTime(2016, 7, 2), 0.5M } };
         public static IEnumerable<object[]> ThreeYears => new[] { new object[] { new DateTime(2015, 1, 1), new DateTime(2018, 1, 1), 3M } };
+        public static IEnumerable<object[]> FiveYears => new[] { new object[] { new DateTime(2015, 1, 1), new DateTime(2020, 1, 1), 5M } };
         public static IEnumerable<object[]> ZeroDay => new[] { new object[] { new DateTime(2015, 1, 1), new DateTime(2015, 1, 1), 0M } };
         public static IEnumerable<object[]> OneDay => new[] { new object[] { new DateTime(2015, 1, 1), new DateTime(2015, 1, 2), 1 / 365M } };
         public static IEnumerable<object[]> TwoDays => new[] { new object[] { new DateTime(2015, 1, 1), new DateTime(2015, 1, 3), 2 / 365M } };
@@ -34,6 +35,7 @@
         [MemberData(nameof(OneYear))]
         [MemberData(nameof(HalfYear))]
         [MemberData(nameof(ThreeYears))]
+        [MemberData(nameof(FiveYears))]
         [MemberData(nameof(StartDateIsMiddleOfYear))]
         [MemberData(nameof(ZeroDay))]
         [MemberData(nameof(OneDay))]
